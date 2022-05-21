50653e0ba423f80d6e4ee2607ad5d9b224975c24
sam <semen.tinchurin@mail.ru>
2022-05-21 13:11:34 +0300

    search by name, displaying tags

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -32,7 +32,18 @@ class PostsByCategory(ListView):
 
 
 class PostsByTag(ListView):
-    pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 
 class GetPost(DetailView):
@@ -45,4 +56,18 @@ class GetPost(DetailView):
         self.object.views = F('views') + 1
         self.object.save()
         self.object.refresh_from_db()
+        return context
+
+
+class Search(ListView):
+    template_name = 'blog/search.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return Post.objects.filter(title__icontains=self.request.GET.get('s'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['s'] = f"s={self.request.GET.get('s')}&"
         return context
